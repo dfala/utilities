ccffb9e99cb507edaf7c78eba1541a3eef120918
Daniel Falabella <dnlfala@gmail.com>
2015-05-27 11:58:38 -0600

    Finished homework

--- src/underscore_tests.js
+++ src/underscore_tests.js
@@ -16,6 +16,19 @@ var _ = { };
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
+    if (n) {
+      var newArray = [];
+      if (n > array.length) {
+        n = array.length;
+      }
+
+      for (var i = 0; i < n; i++) {
+        newArray.push(array[i]);
+      }
+      return newArray;
+    } else {
+      return array[0];
+    }
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
